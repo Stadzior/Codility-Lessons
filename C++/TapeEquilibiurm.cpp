b07b1155b69f798ba04a90cdc0f70d8b8b20eddd
Stadzior <Stadzior@gmail.com>
2015-03-19 19:14:55 +0100

    .

--- C++/TapeEquilibiurm.cpp
+++ C++/TapeEquilibiurm.cpp
@@ -3,18 +3,17 @@ using namespace std;
 
 int solution(vector<int> &A) {
     // write your code in C++11
-    int minimalDifference,Difference=100,SumOfAllElements=0;
-    for(int i=0;i<A.size();i++)
+    int minimalDifference=0,Difference=0,SumOfAllElements=0;
+    for(unsigned int i=0;i<A.size();i++)
     {
         SumOfAllElements+=A[i];
     }
     int LeftSide=0,RightSide=SumOfAllElements;
-    for(int i=0;i<A.size()-1;i++)
+    for(unsigned int i=0;i<A.size()-1;i++)
     {
         LeftSide+=A[i];
         RightSide-=A[i];
         Difference=abs(LeftSide-RightSide);
-        cout<<Difference;
         if((i==0)||(Difference<minimalDifference))
         {
             minimalDifference=Difference;
